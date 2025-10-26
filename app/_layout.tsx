7b740b400ca0d91cbe94326d6a531a4085b115eb
Oliver Mainey <oliverm@trilogycare.com.au>
2025-10-27 09:36:12 +1000

    Switched back to normal login page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,29 @@
 import * as React from "react";
 import { SafeAreaView } from "react-native";
-// import { LoginForm } from "./login/LoginForm";
-// import { Home } from "./Home";
+import { LoginForm } from "./login/LoginForm";
+import { Home } from "./Home";
 import { PaperProvider } from "react-native-paper";
 import { LightTheme, DarkTheme } from "../components/theme";
-import { Appearance } from 'react-native';
-import ThemeSwatch from "./theme/ThemeSwatch";
+import { Appearance } from "react-native";
 
 const App = () => {
   const [user, setUser] = React.useState<string | null>(null);
   const colorScheme = Appearance.getColorScheme();
 
   const theme = {
-    ...(colorScheme === 'dark' ? DarkTheme : LightTheme),
+    ...(colorScheme === "dark" ? DarkTheme : LightTheme),
   };
 
   return (
     <PaperProvider theme={theme}>
-      <SafeAreaView style={{backgroundColor: theme.colors.background, flex: 1}}>
-        <ThemeSwatch />
-        {/* {user == null ? (
+      <SafeAreaView
+        style={{ backgroundColor: theme.colors.background, flex: 1 }}
+      >
+        {user == null ? (
           <LoginForm onLoginSuccess={setUser} />
         ) : (
           <Home user={user} />
-        )} */}
+        )}
       </SafeAreaView>
     </PaperProvider>
   );
